a842d1893cd1055f46b6193151790659d4deb3ae
yctan70 <yctan@yahoo.com>
2023-08-01 21:10:10 +0800

    Added a new knowledge base 'emotion' and more document loaders for building the vectorstore

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from clc.langchain_application import LangChainApplication
 class LangChainCFG:
     llm_model_name = 'THUDM/chatglm2-6b-int4'  # 本地模型文件 or huggingface远程仓库
     embedding_model_name = 'GanymedeNil/text2vec-large-chinese'  # 检索模型文件 or huggingface远程仓库
-    vector_store_path = './cache'
+    vector_store_path = '/content/MyDrive/cache'
     docs_path = './docs'
     kg_vector_stores = {
-        'emotion': './cache/emotion',
-        '中文维基百科': './cache/zh_wikipedia',
-        '大规模金融研报': './cache/financial_research_reports',
-        '初始化': './cache',
+        'emotion': '/content/MyDrive/cache/emotion',
+        '中文维基百科': '/content/MyDrive/cache/zh_wikipedia',
+        '大规模金融研报': '/content/MyDrive/cache/financial_research_reports',
+        '初始化': '/content/MyDrive/cache/cache',
     }  # 可以替换成自己的知识库，如果没有需要设置为None
     # kg_vector_stores=None
     patterns = ['模型问答', '知识库问答']  #
